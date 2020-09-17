cb2b54d1af24ee9d3dec3a08df7199bf41d43e3d
Nafi Zaman <zamannafi3@gmail.com>
2020-09-17 14:14:19 +0200

    Added folders for assignments

---
+++ Where's Croc/Where's Croc/Group 39 Where's Croc.R
@@ -0,0 +1,8 @@
+library(WheresCroc)
+
+# runWheresCroc(randomWC, doPlot = T, showCroc = F, pause = 1,
+#               verbose = T, returnMem = F, mem = NA)
+
+stateMatrix <- matrix(1, nrow = 40, ncol = 40, byrow = T)
+
+
